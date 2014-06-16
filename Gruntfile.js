74c79e3122e32ef3968bdf95f5796ba84d367438
Pierre Kraemer <kraemer@unistra.fr>
2014-06-16 23:23:24 +0200

    move files in client/src/common directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -99,7 +99,7 @@ module.exports = function (grunt) {
             app_common_views : {
                 files : [
                     { // our app common views
-                        cwd : 'client/src/common',
+                        cwd : 'client/src/common/views',
                         src : ['**/*.view.html'],
                         dest : 'public/views/common',
                         expand : true,
